67d0dae4dbd719cc5243a77155c53511573917a0
ParkJuneWoo <krca0220@gmail.com>
2024-06-24 22:13:39 +0900

    add shell

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wabiz_client/router.dart';
 import 'package:flutter_wabiz_client/theme.dart';
 
 void main() {
@@ -10,12 +11,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: wabizDefaultTheme,
-        home: const Scaffold(
-          body: Center(
-            child: Text("Hello, World!"),
-          ),
-        ));
+    return MaterialApp.router(
+      title: "wabiz",
+      routerConfig: router,
+      theme: wabizDefaultTheme,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
+      themeAnimationStyle: AnimationStyle(
+        curve: Curves.easeInCirc,
+        duration: const Duration(milliseconds: 350),
+      ),
+    );
   }
 }
