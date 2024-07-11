09448b0aa68484f614401544219645ea8d209f3c
ParkJuneWoo <krca0220@gmail.com>
2024-07-12 00:02:08 +0900

    add favorite ui

--- lib/views/favorite/favorite_page.dart
+++ lib/views/favorite/favorite_page.dart
@@ -1,10 +1,165 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_wabiz_client/theme.dart';
+import 'package:gap/gap.dart';
 
-class FavoritePage extends StatelessWidget {
+class FavoritePage extends StatefulWidget {
   const FavoritePage({super.key});
 
   @override
+  State<FavoritePage> createState() => _FavoritePageState();
+}
+
+class _FavoritePageState extends State<FavoritePage> {
+  List<String> tabs = [
+    '펀딩',
+    '메이커',
+    '알림신청',
+    '펀딩/프리오더',
+    '스토어',
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                children: [
+                  const Text(
+                    '관심 있는 소식만 모았어요',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.search,
+                      )),
+                ],
+              ),
+            ),
+            Container(
+              height: 32,
+              padding: const EdgeInsets.only(left: 16),
+              child: ListView.builder(
+                itemCount: tabs.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(right: 12),
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    decoration: const BoxDecoration(
+                      color: AppColors.newBg,
+                    ),
+                    child: Center(
+                      child: Text(
+                        tabs[index],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+            const Gap(12),
+            Expanded(
+              child: ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(
+                        left: 16, right: 16, top: 20, bottom: 8),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: const Offset(0, 8),
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 30,
+                          spreadRadius: 4,
+                        )
+                      ],
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          height: 190,
+                          decoration: const BoxDecoration(
+                            color: Colors.amber,
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(10),
+                              topRight: Radius.circular(10),
+                            ),
+                          ),
+                          child: Stack(
+                            children: [
+                              Positioned(
+                                right: 8,
+                                top: 8,
+                                child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(
+                                    Icons.favorite,
+                                  ),
+                                  color: Colors.red,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "n 명이 기다려요",
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700,
+                                  color: AppColors.primaryColor,
+                                ),
+                              ),
+                              const Gap(8),
+                              const Text("놀라운 프로젝트"),
+                              const Gap(24),
+                              Text(
+                                "홍길동",
+                                style: TextStyle(
+                                  color: AppColors.wabizGray[500]!,
+                                ),
+                              ),
+                              const Gap(12),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: AppColors.bg,
+                                  borderRadius: BorderRadius.circular(3),
+                                ),
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 6,
+                                  vertical: 4,
+                                ),
+                                child: const Text('오픈 예정'),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
